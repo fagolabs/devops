cae72bf9b8c792c45a6fb2f254690e119c4c381f
hieunt79 <hieunt79@gmail.com>
2019-08-27 18:28:24 +0700

    Add taiga and update github_practices.md

---
+++ taiga/taiga-server.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+# password of taiga user
+TAIGA_USER_PASS="secretpass"
+# domain name of taiga host
+TAIGA_DOMAIN="example.com"
+# taiga user's home
+TAIGA_HOME="/home/taiga"
+
+# install requirements
+sudo apt-get update
+sudo apt-get install -y build-essential binutils-doc autoconf flex bison libjpeg-dev
+sudo apt-get install -y libfreetype6-dev zlib1g-dev libzmq3-dev libgdbm-dev libncurses5-dev
+sudo apt-get install -y automake libtool curl git tmux gettext
+sudo apt-get install -y nginx
+
+# install postgresql
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+sudo apt update
+sudo apt -y install postgresql-10
+
+# install python and virtualenvwrapper
+sudo apt-get install -y python3 python3-pip python3-dev virtualenvwrapper
+sudo apt-get install -y libxml2-dev libxslt-dev
+sudo apt-get install -y libssl-dev libffi-dev
+
+
+# make encrypt passwd for taiga
+echo $TAIGA_USER_PASS > pass.wd
+ENCRYPT_PASS=$(openssl passwd -crypt -in pass.wd)
+rm pass.wd
+# add user taiga
+useradd -m -p $ENCRYPT_PASS taiga
+adduser taiga sudo
+
+# add user and database postgresql
+sudo -u postgres createuser taiga
+sudo -u postgres createdb taiga -O taiga --encoding='utf-8' --locale=en_US.utf8 --template=template0
+
+# get backend
+cd /home/taiga
+sudo -u taiga git clone https://github.com/taigaio/taiga-back.git taiga-back
+cd taiga-back
+sudo -u taiga git checkout stable
+
+# install backend
+sudo -u taiga mkvirtualenv -p /usr/bin/python3 taiga
+pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py loaddata initial_user
+python manage.py loaddata initial_project_templates
+python manage.py compilemessages
+python manage.py collectstatic --noinput
+
+sudo -u taiga cat > $TAIGA_HOME/taiga-back/settings/local.py << EOF
+from .common import *
+
+MEDIA_URL = "https://$TAIGA_DOMAIN/media/"
+STATIC_URL = "https://$TAIGA_DOMAIN/static/"
+SITES["front"]["scheme"] = "https"
+SITES["front"]["domain"] = "$TAIGA_DOMAIN"
+
+SECRET_KEY = "theveryultratopsecretkey"
+
+DEBUG = False
+PUBLIC_REGISTER_ENABLED = True
+
+DEFAULT_FROM_EMAIL = "no-reply@example.com"
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+#CELERY_ENABLED = True
+
+EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
+EVENTS_PUSH_BACKEND_OPTIONS = {"url": "amqp://taiga:PASSWORD_FOR_EVENTS@localhost:5672/taiga"}
+
+# Uncomment and populate with proper connection parameters
+# for enable email sending. EMAIL_HOST_USER should end by @domain.tld
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_USE_TLS = False
+#EMAIL_HOST = "localhost"
+#EMAIL_HOST_USER = ""
+#EMAIL_HOST_PASSWORD = ""
+#EMAIL_PORT = 25
+
+# Uncomment and populate with proper connection parameters
+# for enable github login/singin.
+#GITHUB_API_CLIENT_ID = "yourgithubclientid"
+#GITHUB_API_CLIENT_SECRET = "yourgithubclientsecret"
+EOF
+
+# install frontend
+cd $TAIGA_HOME
+sudo -u taiga git clone https://github.com/taigaio/taiga-front-dist.git taiga-front-dist
+cd taiga-front-dist
+sudo -u taiga git checkout stable
+
+sudo -u taiga cat > $TAIGA_HOME/taiga-front-dist/dist/conf.json << EOF
+{
+    "api": "https://$TAIGA_DOMAIN/api/v1/",
+    "eventsUrl": null,
+    "eventsMaxMissedHeartbeats": 5,
+    "eventsHeartbeatIntervalTime": 60000,
+    "eventsReconnectTryInterval": 10000,
+    "debug": true,
+    "debugInfo": false,
+    "defaultLanguage": "en",
+    "themes": ["taiga"],
+    "defaultTheme": "taiga",
+    "publicRegisterEnabled": true,
+    "feedbackEnabled": true,
+    "supportUrl": "https://tree.taiga.io/support/",
+    "privacyPolicyUrl": null,
+    "termsOfServiceUrl": null,
+    "GDPRUrl": null,
+    "maxUploadFileSize": null,
+    "contribPlugins": [],
+    "tribeHost": null,
+    "importers": [],
+    "gravatar": true,
+    "rtlLanguages": ["fa"]
+EOF
+
+# run taiga with systemd
+cat > /etc/systemd/system/taiga.serivce << EOF
+[Unit]
+Description=taiga_back
+After=network.target
+
+[Service]
+User=taiga
+Environment=PYTHONUNBUFFERED=true
+WorkingDirectory=/home/taiga/taiga-back
+ExecStart=/home/taiga/.virtualenvs/taiga/bin/gunicorn --workers 4 --timeout 60 -b 127.0.0.1:8001 taiga.wsgi
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=default.target
+EOF
+
+# expose with nginx
+sudo systemctl daemon-reload
+sudo systemctl start taiga
+sudo systemctl enable taiga
+
+sudo rm /etc/nginx/sites-enabled/default
+mkdir -p $TAIGA_HOME/logs
+
+cat > /etc/nginx/conf.d/taiga.conf << EOF
+server {
+    listen 80 default_server;
+    server_name _;
+
+    large_client_header_buffers 4 32k;
+    client_max_body_size 50M;
+    charset utf-8;
+
+    access_log /home/taiga/logs/nginx.access.log;
+    error_log /home/taiga/logs/nginx.error.log;
+
+    # Frontend
+    location / {
+        root /home/taiga/taiga-front-dist/dist/;
+        try_files \$uri \$uri/ /index.html;
+    }
+
+    # Backend
+    location /api {
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Scheme \$scheme;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_pass http://127.0.0.1:8001/api;
+        proxy_redirect off;
+    }
+
+    # Admin access (/admin/)
+    location /admin {
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Scheme \$scheme;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_pass http://127.0.0.1:8001\$request_uri;
+        proxy_redirect off;
+    }
+
+    # Static files
+    location /static {
+        alias /home/taiga/taiga-back/static;
+    }
+
+    # Media files
+    location /media {
+        alias /home/taiga/taiga-back/media;
+    }
+
+    # Events
+    location /events {
+        proxy_pass http://127.0.0.1:8888/events;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_connect_timeout 7d;
+        proxy_send_timeout 7d;
+        proxy_read_timeout 7d;
+	}
+}
+EOF
